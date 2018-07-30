991c9d4d10e201efb5acbac2b3e05086a7e855d9
Marlon Mendes <marlonciriatico@gmail.com>
2018-07-30 11:31:32 -0300

    Finding outliers in total verified costs
    Signed-off-by: Marlon Mendes <marlonciriatico@gmail.com>
    Co-authored-by: Alexandre TK <alexandrekry@gmail.com>

--- salicml/outliers/gaussian_outlier.py
+++ salicml/outliers/gaussian_outlier.py
@@ -1,7 +1,7 @@
 import scipy
 
 def is_outlier(x, mean, standard_deviation, c = 1.5):
-    return x > mean + c * standard_deviation
+    return x > (mean + c * standard_deviation)
 
 def outlier_probability(mean, standard_deviation, c = 1.5):
     norm = scipy.stats.norm
