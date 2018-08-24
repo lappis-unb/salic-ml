3238eb42929ed91202858c3538eb4cd59b8f7031
Marlon Mendes <marlonciriatico@gmail.com>
2018-08-24 13:05:21 -0300

    Updating ftp with downloaded .csv files
    Signed-off-by: Marlon Mendes <marlonciriatico@gmail.com>
    Co-authored-by: Lucas Mattioli <lucas.mattioli@live.com>

--- data/scripts/csv_ftp_updater.py
+++ data/scripts/csv_ftp_updater.py
@@ -7,10 +7,7 @@ import sys
 import pandas as pd
 import pyodbc
 
-
-def debug(a, b):
-    print('{} = {}'.format(a, b))
-    print('type({}) = {}'.format(a, type(b)))
+from ftplib import FTP
 
 CREDENTIALS = {
     'HOST': os.environ.get('DB_HOST', ''),
@@ -18,17 +15,9 @@ CREDENTIALS = {
     'USER': os.environ.get('DB_USER', ''),
     'PASSWORD': os.environ.get('DB_PASSWORD', ''),
     'DATABASE': os.environ.get('DB_NAME', ''),
+    'FTP_USER': os.environ.get('FTP_USER', ''),
+    'FTP_PASSWORD': os.environ.get('FTP_PASSWORD', ''),
 }
-
-def execute_pandas_sql_query(query):
-    '''Executes SQL query, returns the result as a DataFrame object.'''
-    db_parameters = 'DRIVER=FreeTDS;SERVER={0};PORT={1};DATABASE=;UID={2};PWD={3};TDS_Version=8.0;'.format(CREDENTIALS['HOST'], CREDENTIALS['PORT'], CREDENTIALS['USER'], CREDENTIALS['PASSWORD'])
-
-    db_conection = pyodbc.connect(db_parameters)
-    dataframe = pd.read_sql_query(query, db_conection)
-    db_conection.close()
-
-    return dataframe
 
 def set_and_parse_args():
     DESCRIPTION = 'Executes SQL Queries on SALIC database, ' \
@@ -51,13 +40,23 @@ def set_and_parse_args():
     args_dict = vars(args)
     return args_dict
 
+def execute_pandas_sql_query(query):
+    '''Executes SQL query, returns the result as a DataFrame object.'''
+    db_parameters = 'DRIVER=FreeTDS;SERVER={0};PORT={1};DATABASE=;UID={2};PWD={3};TDS_Version=8.0;'.format(CREDENTIALS['HOST'], CREDENTIALS['PORT'], CREDENTIALS['USER'], CREDENTIALS['PASSWORD'])
+
+    db_conection = pyodbc.connect(db_parameters)
+    dataframe = pd.read_sql_query(query, db_conection)
+    db_conection.close()
+
+    return dataframe
+
 def save_dataframe_as_csv(dt, dir_path):
     if not isinstance(dt, pd.DataFrame):
         raise ValueError('dt argument must be a ' \
                 '{}'.format(pd.DataFrame.__name__))
 
     if os.path.exists(dir_path):
-        answer = input('Overwrite file/dir \'{}\'?[y/N]'.format(dir_path))
+        answer = input('Overwrite file/dir \'{}\'?[y/N] '.format(dir_path))
         if answer != 'y':
             exit('Abort.')
 
@@ -65,8 +64,28 @@ def save_dataframe_as_csv(dt, dir_path):
     if not os.path.exists(os.path.dirname(dir_path)):
         os.makedirs(dirname)
 
-    dt.to_csv(dir_path, encoding='utf-8')
-    print('.csv saved in file path \'{}\'.'.format(dir_path))
+    dt.to_csv(dir_path, encoding='utf-8', index=False)
+    print('.csv saved in path \'{}\'.'.format(dir_path))
+
+def save_file_in_ftp(ftp, source_file_path, dest_file_path):
+    dest_filename = os.path.basename(dest_file_path)
+    dest_dirname = os.path.dirname(dest_file_path)
+    ftp.cwd(dest_dirname)
+    with open(source_file_path, 'rb') as f_send:
+        ftp.storbinary('STOR {}'.format(dest_filename), f_send)
+        ftp.sendcmd('SITE CHMOD 644 ' + dest_filename)
+        print('File \'{}\' saved in \'ftp/{}\''.format(source_file_path,
+            dest_file_path))
+
+def init_ftp():
+    host = '178.128.12.141'
+    user = CREDENTIALS['FTP_USER']
+    passwd = CREDENTIALS['FTP_PASSWORD']
+
+    ftp = FTP(host)
+    ftp.login(user, passwd)
+
+    return ftp
 
 def main():
     args = set_and_parse_args()
@@ -86,6 +105,7 @@ def main():
         else:
             print('Ignoring non-sql file {}'.format(path))
 
+    csv_paths = []
     for file_path in files:
         with open(file_path, 'r') as file_content:
             query = file_content.read()
@@ -93,6 +113,19 @@ def main():
 
             csv_path = os.path.join(csv_dir, file_path[:-4] + '.csv')
             save_dataframe_as_csv(query_result, csv_path)
+            
+            csv_paths.append(csv_path)
+    
+    ftp = init_ftp()
+
+    update_ftp = args['update_ftp']
+    if update_ftp:
+        for csv_path in csv_paths:
+            csv_filename = os.path.basename(csv_path)
+            dest_file_path = 'raw/' + csv_filename
+            save_file_in_ftp(ftp, csv_path, dest_file_path)
+
+    ftp.quit()
 
 if __name__ == '__main__':
     main()
