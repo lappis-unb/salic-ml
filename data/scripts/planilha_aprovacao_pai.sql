22b56c2f045469e818e71521727c0cf3807b341a
Marlon Mendes <marlonciriatico@gmail.com>
2018-08-28 17:44:49 -0300

    Guide for using csv_ftp_update.py script, updating planilha_aprovacao_pai.sql

--- data/scripts/planilha_aprovacao_pai.sql
+++ data/scripts/planilha_aprovacao_pai.sql
@@ -1,17 +1,14 @@
-SELECT a.idPronac,a.AnoProjeto+a.Sequencial as PRONAC,a.NomeProjeto,k.idProduto,k.idPlanilhaAprovacao, k.idPlanilhaAprovacaoPai,
-       case 
-         when k.idProduto = 0
-           then 'Administracao do Projeto'
-           else c.Descricao 
-         end as Produto,
-       convert(varchar(8),d.idPlanilhaEtapa) + ' - ' + d.Descricao as Etapa,i.Descricao as Item,
-       (z.Quantidade * z.Ocorrencia * z.ValorUnitario) as VlSolicitado,z.dsJustificativa as JustProponente,
-       (b.Quantidade * b.Ocorrencia * b.ValorUnitario) as VlSugerido,b.Justificativa as JustParecerista,
-       e.Descricao as Unidade,k.QtItem,k.nrOcorrencia,k.VlUnitario,k.QtDias,
-       k.TpDespesa,k.TpPessoa,k.nrContrapartida,k.nrFonteRecurso as idFonte,x.Descricao as FonteRecurso,f.UF,
+SELECT a.idPronac,a.AnoProjeto+a.Sequencial as PRONAC,k.idPlanilhaAprovacao, k.idPlanilhaAprovacaoPai,i.Descricao as Item,
+       (z.Quantidade * z.Ocorrencia * z.ValorUnitario) as VlSolicitado,
+       (b.Quantidade * b.Ocorrencia * b.ValorUnitario) as VlSugerido,
+       e.Descricao as Unidade,k.QtItem,k.nrOcorrencia,k.VlUnitario,k.QtDias,f.UF,
        f.Municipio,
        ROUND((k.QtItem * k.nrOcorrencia * k.VlUnitario),2) as Aprovado,
-       k.dsJustificativa as JustComponente
+       k.dtPlanilha,
+       k.stAtivo,
+       projetos.Area,
+       projetos.Segmento,
+       projetos.DtProtocolo AS DataProjeto
        FROM SAC.dbo.Projetos a
        INNER JOIN SAC.dbo.tbPlanilhaProjeto b on (a.idPronac = b.idPronac)
        INNER JOIN SAC.dbo.tbPlanilhaProposta z on (b.idPlanilhaProposta=z.idPlanilhaProposta)
@@ -21,5 +18,5 @@ SELECT a.idPronac,a.AnoProjeto+a.Sequencial as PRONAC,a.NomeProjeto,k.idProduto,
        INNER JOIN SAC.dbo.tbPlanilhaUnidade e on (b.idUnidade = e.idUnidade)
        INNER JOIN SAC.dbo.tbPlanilhaItens i on (b.idPlanilhaItem=i.idPlanilhaItens)
        INNER JOIN SAC.dbo.Verificacao x on (b.FonteRecurso = x.idVerificacao)
-       INNER JOIN agentes.dbo.vUfMunicipio f on (b.UfDespesa = f.idUF and b.MunicipioDespesa = f.idMunicipio);
-
+       INNER JOIN agentes.dbo.vUfMunicipio f on (b.UfDespesa = f.idUF and b.MunicipioDespesa = f.idMunicipio)
+       INNER JOIN SAC.dbo.Projetos projetos on (a.idPronac = projetos.IdPRONAC);
