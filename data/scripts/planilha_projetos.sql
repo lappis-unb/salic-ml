ca14544f94b4ccc69aa40a75aaeb831c1703eb1b
Marlon Mendes <marlonciriatico@gmail.com>
2018-10-10 17:32:46 -0300

    Removing unnecessary columns

--- data/scripts/planilha_projetos.sql
+++ data/scripts/planilha_projetos.sql
@@ -1,3 +1,15 @@
-SELECT projetos.AnoProjeto + projetos.Sequencial as PRONAC, *
-FROM SAC.dbo.Projetos projetos
+SELECT projetos.AnoProjeto + projetos.Sequencial as PRONAC,
+idPRONAC,
+UfProjeto,
+Area,
+Segmento,
+NomeProjeto,
+CgcCpf,
+Situacao,
+DtProtocolo,
+DtSituacao,
+DtInicioExecucao,
+DtFimExecucao
+FROM SAC.dbo.Projetos
 ;
+
