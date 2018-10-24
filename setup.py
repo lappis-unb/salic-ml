18a8eca0c75125a95462e6296ba4b9d5be4c657c
Victor Moura <victor_cmoura@hotmail.com>
2018-10-24 14:28:49 -0300

    Updating salic-ml pip version
    Co-authored-by: Felipe Duerno <felipeduernoal@gmail.com>

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='salic-ml',
-    version='0.0.10',
+    version='0.0.11',
     description='Automate the Salic proposal admission process',
     url='https://github.com/lappis-unb/salic-ml',
     license='GPL v3.0',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
-        'click', 'jupyter', 'matplotlib', 'numpy', 'pandas', 'requests',
-        'scikit-learn', 'scipy'
+        'click', 'numpy', 'pandas', 'requests',
+        'scikit-learn', 'scipy', 'pyodbc'
     ],
     python_requires='>=3',
 )
