b66dae96daf1429d6937934deee76f1b2ba135a3
rodrigocam <rodrigo.redcode@gmail.com>
2019-01-08 12:15:29 -0200

    Add pickle inv task

--- tasks.py
+++ tasks.py
@@ -1,6 +1,12 @@
 from invoke import task
+from src.salicml.data import csv_to_pickle
 
 
 @task
 def hello(ctx, name='world'):
     ctx.run(f'echo "hello {name}"')
+
+
+@task
+def pickle(ctx, clean=False):
+    csv_to_pickle(clean=clean)
