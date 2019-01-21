6ed58a6a9273eac0cf52aea65f697fb74e9f0c8a
rodrigocam <rodrigo.redcode@gmail.com>
2019-01-21 11:09:29 -0200

    Update approved_funds metric tests

--- tests/finance/test_approved_funds.py
+++ tests/finance/test_approved_funds.py
@@ -1,17 +1,14 @@
 import unittest
 
 from salicml.data.query import metrics
-from salicml.metrics.finance.approved_funds import approved_funds
-
+from salicml.metrics.finance import approved_funds
 
 class TestApprovedFunds(unittest.TestCase):
-    def setUp(self):
-        metrics.register(approved_funds)
 
     def test_inlier_pronac(self):
         project = metrics.get_project('138140')
-        self.assertFalse(project.finance.approved_funds['is_outlier'])
+        assert not project.finance.approved_funds['is_outlier']
 
     def test_outlier_pronac(self):
         project = metrics.get_project('121386')
-        self.assertTrue(project.finance.approved_funds['is_outlier'])
+        assert project.finance.approved_funds['is_outlier'] 
