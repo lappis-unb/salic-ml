25ae08d283b4a96a304f15bb0820ad3f1d8d6fcd
rodrigocam <rodrigo.redcode@gmail.com>
2019-02-06 16:27:41 -0200

    Fix proponent_projects metric tests

--- tests/finance/test_proponent_projects.py
+++ tests/finance/test_proponent_projects.py
@@ -9,55 +9,32 @@ class TestProponentProjects(unittest.TestCase):
         pronac = "1011645"  # CGCCPF -> 00367651000114
         project = metrics.get_project(pronac)
 
-        # # Testing cached dictionary
-        # analyzed_projects = self.proponent_projects.analyzed_projects
-        # submitted_projects = self.proponent_projects.submitted_projects
-
-        # pronac_keys = ["pronacs_list", "num_pronacs"]
-
-        # for key in analyzed_projects.keys():
-        #     map(lambda x: self.assertIn(x, analyzed_projects[key]), pronac_keys)
-
-        # for key in analyzed_projects.keys():
-        #     map(lambda x: self.assertIn(x, submitted_projects[key]), pronac_keys)
-
-        # Testing get metric
+        # Testing metric
         proponent_pronacs = project.finance.proponent_projects
-        
-        self.assertGreater(
-            proponent_pronacs["analyzed_projects"]["number_of_projects"], 0
-        )
-        self.assertTrue(
-            any(proponent_pronacs["analyzed_projects"]["pronacs_of_this_proponent"])
-        )
-        self.assertIn(
-            pronac, proponent_pronacs["analyzed_projects"]["pronacs_of_this_proponent"]
+
+        assert proponent_pronacs['analyzed_projects']['number_of_projects'] > 0
+
+        assert any(
+            proponent_pronacs['analyzed_projects']['pronacs_of_this_proponent']
         )
 
-    # def test_proponent_with_submitted_project(self):
-    #     pronac = "079172"  # CGCCPF -> '00274080001'
+        assert pronac in (
+            proponent_pronacs['analyzed_projects']['pronacs_of_this_proponent']
+        )
 
-    #     # Testing cached dictionary
-    #     analyzed_projects = self.proponent_projects.analyzed_projects
-    #     submitted_projects = self.proponent_projects.submitted_projects
+    def test_proponent_with_submitted_project(self):
+        pronac = "079172"  # CGCCPF -> '00274080001'
+        project = metrics.get_project(pronac)
 
-    #     pronac_keys = ["pronacs_list", "num_pronacs"]
+        # Testing metric
+        proponent_pronacs = project.finance.proponent_projects
 
-    #     for key in analyzed_projects.keys():
-    #         map(lambda x: self.assertIn(x, analyzed_projects[key]), pronac_keys)
+        assert proponent_pronacs['submitted_projects']['number_of_projects'] > 0
 
-    #     for key in analyzed_projects.keys():
-    #         map(lambda x: self.assertIn(x, submitted_projects[key]), pronac_keys)
+        assert any(
+            proponent_pronacs['submitted_projects']['pronacs_of_this_proponent']
+        )
 
-    #     # Testing get metric
-    #     proponent_pronacs = self.proponent_projects.get_metrics(pronac)
-
-    #     self.assertGreater(
-    #         proponent_pronacs["submitted_projects"]["number_of_projects"], 0
-    #     )
-    #     self.assertTrue(
-    #         any(proponent_pronacs["submitted_projects"]["pronacs_of_this_proponent"])
-    #     )
-    #     self.assertIn(
-    #         pronac, proponent_pronacs["submitted_projects"]["pronacs_of_this_proponent"]
-    #     )
+        assert pronac in (
+            proponent_pronacs['submitted_projects']['pronacs_of_this_proponent']
+        )
