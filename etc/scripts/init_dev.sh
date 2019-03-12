6bf31372460884c0c778faa6eaf64da1ecd8a11b
pablodiegoss <pablodiegoss@hotmail.com>
2019-03-12 11:51:41 -0300

    Update Dockerfile to use the base image with requirements

--- etc/scripts/init_dev.sh
+++ etc/scripts/init_dev.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-pip3 install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
+ls
 echo "** Attempting to start service **"
-python3 manage.py runserver 0.0.0.0:8080
+inv update-data
+
+# cd src/api/
+# python3 manage.py makemigrations
+# python3 manage.py migrate
+
+
+# python3 manage.py runserver 0.0.0.0:8080
