5d35ddebc5cd9efe25f1a88c19d76c6114fdac76
pablodiegoss <pablodiegoss@hotmail.com>
2019-03-14 12:03:45 -0300

    Update Dockerfile

--- etc/scripts/init_dev.sh
+++ etc/scripts/init_dev.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-ls
+
+cd src/api/
+python3 manage.py makemigrations
+python3 manage.py migrate
+
 echo "** Attempting to start service **"
-inv update-data
-
-# cd src/api/
-# python3 manage.py makemigrations
-# python3 manage.py migrate
-
-
-# python3 manage.py runserver 0.0.0.0:8080
+python3 manage.py runserver 0.0.0.0:8080
