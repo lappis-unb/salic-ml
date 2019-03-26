c21c36c6b1bca72260e6f0ac2a4fc029b8c9c61a
Fabíola Fleury <fabiola.mfleury@gmail.com>
2019-03-26 09:58:29 -0300

    Configure custom pagination to format according to frontend

--- src/api/config/settings.py
+++ src/api/config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.config.pagination.CustomPagination',
     'PAGE_SIZE': 15
 }
 
