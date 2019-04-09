5f140d2e979141a3a646dbebdcaf026090cfa90e
Fabíola Fleury <fabiola.mfleury@gmail.com>
2019-04-09 13:32:21 -0300

    Implement metric by receipts in salic ml and filter notebook information about it

--- src/salicml/metrics/finance/operation_code_receipts.py
+++ src/salicml/metrics/finance/operation_code_receipts.py
@@ -1,21 +1,46 @@
 from salicml.data.query import metrics
 from salicml.data import data
+import numpy as np
 
-COLUMNS = ["PRONAC", "Item", "vlAprovado", "vlComprovacao"]
+COLUMNS = ['PRONAC', 'idComprovantePagamento', 'tpFormaDePagamento', 'Item',
+           'nmFornecedor', 'vlComprovacao']
+new_tpFormaDePagamento = {0.0: np.nan, 1.0: "Cheque",
+                          2.0: "Transferência Bancária", 3.0: 'Saque/Dinheiro'}
 
 
 @metrics.register('finance')
 def operation_code_receipts(pronac, dt):
-    pass
+    df = data.verified_repeated_receipts
+    df = df[df['PRONAC'] == pronac]
+    comprovantes_cheque = df[df['tpFormaDePagamento'] == 1.0]
+    comprovantes_transferencia = df[df['tpFormaDePagamento'] == 2.0]
+    comprovantes_saque = df[df['tpFormaDePagamento'] == 3.0]
+
+    return {
+        "receipts_check": metric_return(comprovantes_cheque),
+        "receipts_transfer": metric_return(comprovantes_transferencia),
+        "receipts_money": metric_return(comprovantes_saque),
+    }
 
 
 @data.lazy('planilha_comprovacao')
-def approved_verified_items(df):
-    receipts = (df[['idComprovantePagamento', 'PRONAC']]
-                .groupby('idComprovantePagamento').count())
-    repeated_receipts = receipts[receipts['PRONAC'] > 1].index.values
-    lines = df.loc[df['idComprovantePagamento'].isin(repeated_receipts)]
+def verified_repeated_receipts(df):
+    receipts = df[COLUMNS]
+    duplicated = receipts[receipts
+                          .duplicated(subset=['idComprovantePagamento'],
+                                      keep=False)]
+    return duplicated
 
-    outliers_pronac = (linesu.groupby(['PRONAC'])
-                       .nunique().index.values)
-    return df[COLUMNS]
+
+def metric_return(dataframe):
+    is_outlier = None
+    del dataframe['tpFormaDePagamento']
+    if dataframe.empty:
+        is_outlier = False
+        dataframe = []
+    else:
+        is_outlier = True
+    return {
+        'is_outlier': is_outlier,
+        'itens_que_compartilham_comprovantes': dataframe,
+    }
