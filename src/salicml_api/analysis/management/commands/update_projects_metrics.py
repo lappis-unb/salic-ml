22bbd33c0ef415bb115c347b8e0208b20e13376c
rodrigocam <rodrigo.redcode@gmail.com>
2019-05-27 15:05:07 -0300

    Fix creation of admissibility indicator with all financial metrics

--- src/salicml_api/analysis/management/commands/update_projects_metrics.py
+++ src/salicml_api/analysis/management/commands/update_projects_metrics.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand
 from analysis.preload_data import load_project_metrics
+from analysis.models import FinancialIndicator, AdmissibilityIndicator
 
 
 class Command(BaseCommand):
     help = 'Updates all saved projects finance metrics and indicator'
 
     def handle(self, *args, **kwargs):
-        load_project_metrics()
+        load_project_metrics(FinancialIndicator)
+        load_project_metrics(AdmissibilityIndicator)
         self.stdout.write("Finished updating projects metrics")
