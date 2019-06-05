2e89c6cef92df5a131836b1cc6cff4c868c46674
Victor Moura <victor_cmoura@hotmail.com>
2019-06-05 16:53:14 -0300

    Fix cronjob context problems
    Co-authored-by: Joenio Costa <joenio@joenio.me>

--- etc/scripts/preload_data.sh
+++ etc/scripts/preload_data.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-echo "Attempting to cron"
-inv get-pickles
-inv update-models train-metrics
-
+echo "======================= $(date) ======================="
+echo "Recalculating metrics"
+cd /
+cat /etc/env/django.env /etc/env/postgres.env > /cronvars.env
+. /cronvars.env /usr/local/bin/inv get-pickles
+. /cronvars.env /usr/local/bin/inv update-models train-metrics
+rm /cronvars.env
