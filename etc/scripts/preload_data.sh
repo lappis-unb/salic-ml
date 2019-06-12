0b9762c58bed6f6fa4e20da423f97b5969bbc1d1
Joenio Costa <joenio@joenio.me>
2019-06-12 20:16:29 -0300

    cron job works partially

--- etc/scripts/preload_data.sh
+++ etc/scripts/preload_data.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 echo "======================= $(date) ======================="
-echo "Recalculating metrics"
 cd /
-cat /etc/env/django.env /etc/env/postgres.env > /cronvars.env
-. /cronvars.env /usr/local/bin/inv get-pickles
-. /cronvars.env /usr/local/bin/inv update-models train-metrics
-rm /cronvars.env
+cat /etc/env/*.env | sed '/^$/d' > /cronvars.env
+cat /cronvars.env | sed 's/^/export /' > /exportvars.env
+. /exportvars.env
+. /cronvars.env
+echo "Recalculating metrics"
+/usr/local/bin/inv get-pickles && /usr/local/bin/inv update-models train-metrics
+echo "Done"
+exit 0
