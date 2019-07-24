fa7783d1503d3241551e9f0f0bdc998bf8d9a6a3
rodrigocam <rodrigo.redcode@gmail.com>
2019-07-24 17:38:56 -0300

    Setup django db for tests

--- src/salicml_api/analysis/tests/conftest.py
+++ src/salicml_api/analysis/tests/conftest.py
@@ -3,15 +3,25 @@ from django.test.client import Client
 from rest_framework.test import APIClient
 from django.db import connection
 import os
+from django.core.management import call_command
 
 @pytest.fixture
 def api_client():
     return APIClient()
 
+#@pytest.fixture(scope='session')
+#def django_db_modify_db_settings():
+#    os.environ['TEST_DB'] = "True"
+#    os.system("inv manager test") 
+#    os.system("inv db-test")
+#    os.system("inv set-data")
+#    os.system("inv train-metrics")
+#    os.environ['TEST_DB'] = "False"
+
 @pytest.fixture(scope='session')
-def django_db_modify_db_settings():
-    os.environ['TEST_DB'] = "True"
-    os.system("inv db-test")
-    os.system("inv set-data")
-    os.system("inv train-metrics")
-    os.environ['TEST_DB'] = "False"
+def django_db_setup(django_db_setup, django_db_blocker):
+        with django_db_blocker.unblock():
+            call_command('loaddata', '/data/dump/dev_project.json')
+            os.system("inv set-data")
+            os.system("inv train_metrics")
+
