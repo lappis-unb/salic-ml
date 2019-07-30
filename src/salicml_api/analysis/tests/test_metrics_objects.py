7e4c26a58167eeb11ec0b5439503c4e53a2df7c5
Victor Moura <victor_cmoura@hotmail.com>
2019-07-30 15:07:50 -0300

    Create initial details url api tests
    Co-authored-by: Joao Guilherme <joaaogui@gmail.com>
    Co-authored-by: Rodrigo Oliveira <rodrigo.redcode@gmail.com>

--- src/salicml_api/analysis/tests/test_metrics_objects.py
+++ src/salicml_api/analysis/tests/test_metrics_objects.py
@@ -1,13 +1,54 @@
 import json
 from .utils import load_json
 
-ROOT_URL = '/'
+PROJECT_DETAIL_URL_132955 = '/v1/projects/132955/details/'
+PROJECT_NAME_132955 = '''
+    Brasil contemporâneo: economia e cultura - Milagre econômico e 
+    recessão nos anos 70/80: décadas de chumbo, contestação e rock brazuca
+    '''
 
-def test_root_url(db, api_client):
-    response = api_client.get(ROOT_URL)
-
+def test_detail_url(db, api_client):
+    response = api_client.get(PROJECT_DETAIL_URL_132955)
     json_data = load_json(response)
 
-    print(json_data)
-
     assert(response.status_code == 200)
+    assert(json_data['pronac'] == '132955')
+    assert(json_data['nome'] == PROJECT_NAME_132955)
+    assert('AdmissibilityIndicator' in json_data['indicadores'])
+    assert('FinancialIndicator' in json_data['indicadores'])
+
+def test_financial_indicator_content(db, api_client):
+    pass
+
+def test_admissibility_indicator_content(db, api_client):
+    pass
+
+def test_itens_comuns_e_incomuns_por_segmento(db, api_client):
+    pass
+
+def test_comprovante_saque(db, api_client):
+    pass
+
+def test_comprovante_cheque(db, api_client):
+    pass
+
+def test_comprovante_transferencia(db, api_client):
+    pass
+
+def test_comprovante_pagamento(db, api_client):
+    pass
+
+def test_itens_orcamentarios(db, api_client):
+    pass
+
+def test_novos_fornecedores(db, api_client):
+    pass
+
+def test_comprovantes_acima_50(db, api_client):
+    pass
+
+def test_projetos_mesmo_proponente(db, api_client):
+    pass
+
+def test_valor_a_ser_comprovado(db, api_client):
+    pass
